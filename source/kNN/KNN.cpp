2566430705edcc1a04e2381f7cc78e89e684a1da
Tom de Ruijter <t.deruijter@deskbookers.com>
2011-02-23 21:56:04 +0100

    Added stub files for kNN

---
+++ source/kNN/KNN.cpp
@@ -0,0 +1,32 @@
+/*
+ *  KNN.cpp
+ *  
+ *
+ *  Created by Tom de Ruijter on 2/21/11.
+ *  Copyright 2011 __MyCompanyName__. All rights reserved.
+ *
+ */
+
+#include "KNN.h"
+
+KNN::KNN(Dataset *dataset)
+{
+	
+}
+
+KNN::~KNN()
+{
+}
+
+std::string KNN::classify(Dataset::Stimulus *target)
+{
+	int *nearestK = new int[k];		//Sorted list with k Nearest points from target.
+	double *distK = new double[k];	//Distances to k Nearest points from target.
+	
+	Dataset::Stimulus *stimuli = dataset.getStimuli();
+	
+	for(int i = 0; i < dataset->getFeatureCount(); i++)
+	{
+		double distance = stimuli[i]->dist(target);
+	}
+}
